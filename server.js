6936c773ab223a4aa028a2e409e4dd2b7e88b172
sumitDevs <mysumitdevs@gmail.com>
2025-02-19 06:35:41 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 import router from './routes/homeRoutes.js';
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);import dotenv from 'dotenv';
 const app = express();
 const PORT  = process.env.PORT;
 connectDB();
